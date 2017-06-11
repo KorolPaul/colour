ea1ae7e0e7056b7d74a812f1e0fdb51249470f9a
CSSKing <korol.paul@gmail.com>
2017-06-11 18:56:51 +0300

    tags

--- renderer.js
+++ renderer.js
@@ -9,10 +9,15 @@ const explorerTrigger = document.getElementById('explorer-trigger'),
       addPopup = document.getElementById('add-popup'),
       addColorTrigger = document.getElementById('add-color'),
       colorsHolder = document.getElementById('colors-holder'),
+      tagsHolder = document.getElementById('tags-holder'),
       triggersHolder = document.getElementById('triggers-holder'),
-      colorInput = document.getElementById('color-code');
+      colorInput = document.getElementById('color-code'),
+      contextMenu = document.getElementById('context'),
+      tagInput = document.getElementById('tag-input'),
+      tagsList = document.getElementById('tags-list');
 
-let colorsJSON = [];
+let colorsJSON = [],
+    selectedColor = 0;
 
 function init() {
     menuTrigger.addEventListener('click', function () {
@@ -41,9 +46,13 @@ function init() {
         ipc.send('open-file-dialog')
     })
 
+    document.body.addEventListener('click', hideContextMenu);    
+
     ipc.on('selected-directory', function (event, path) {
         document.getElementById('selected-file').innerHTML = `You selected: ${path}`
     })
+
+    tagInput.addEventListener('keyup', saveTag);
 
     loadColors();
 }
@@ -54,26 +63,23 @@ function renderColor(colorCode) {
     let el = document.createElement('div');
     el.classList.add('color');
     el.style.background = colorCode;
-    
-    colorsHolder.appendChild(el);
-}
+    el.addEventListener('contextmenu', showContextMenu);
 
-function renderTag(color) {
-    let el = document.createElement('div');
-    el.classList.add('tag');
-    el.style.background = color.color;
-    
-    tagsHolder.appendChild(el);
+    colorsHolder.appendChild(el);
 }
 
 function saveColor(colorCode) {
     let colorObj = {
         color: colorCode,
-        tags: 'test'
+        tags: []
     }
 
     colorsJSON.push(colorObj);
     
+    saveJSON();
+}
+
+function saveJSON() {
     fs.writeFile('colors.json', JSON.stringify(colorsJSON), (err) => {
         if (err) {
             console.log("An error ocurred creating the file " + err.message);
@@ -92,10 +98,99 @@ function loadColors(file) {
 
         colorsJSON.forEach(function (el) {
             renderColor(el.color);
-            renderTag(el);
         });
-        
+
+        loadTags();
     });
+}
+
+function showContextMenu(e) {
+    hideContextMenu(e);
+
+    selectedColor = getIndex(e.target)
+
+    tagsList.innerHTML = '';    
+    colorsJSON[selectedColor].tags.forEach(function (el) {
+        let tag = document.createElement('span');
+        tag.classList.add('tag');
+        tag.innerText = el;
+
+        tagsList.appendChild(tag);
+    });
+
+    contextMenu.style.left = e.target.offsetLeft + e.target.offsetWidth - 15 + 'px';
+    contextMenu.style.top = e.target.offsetTop + e.target.offsetHeight - 15 + 'px';
+    contextMenu.classList.add('visible');
+}
+
+function hideContextMenu(e) {
+    if (!e.target.matches('#context ,#context *')) {
+        contextMenu.classList.remove('visible');
+    }
+}
+
+function saveTag(e) {
+    if (e.code == 'Enter') {
+        let tag = document.createElement('span');
+        tag.classList.add('tags-list_item');
+        tag.innerText = e.target.value;
+
+        tagsList.appendChild(tag);
+
+        colorsJSON[selectedColor].tags.push(e.target.value)
+        saveJSON();
+    }
+}
+
+function loadTags() {
+    let tagsNames = [];
+
+    colorsJSON.forEach(function (el) {
+        el.tags.forEach(function (el) {
+            if (tagsNames.indexOf(el) === -1) {
+                tagsNames.push(el);
+                renderTag(el);
+            }
+        });
+    });
+}
+
+function renderTag(tagName) {
+    let tag = document.createElement('div');
+    tag.classList.add('tag');
+
+    let tagTitle = document.createElement('p');
+    tagTitle.classList.add('tag_title');
+    tagTitle.innerText = tagName;
+
+    let tagColors = document.createElement('div');
+    tagColors.classList.add('tag_colors');
+
+    colorsJSON.forEach(function (el) {
+        for (let i = 0; i < el.tags.length; i++) {
+            if (el.tags[i] === tagName) {
+                let tagColor = document.createElement('div');
+                tagColor.style.backgroundColor = el.color;
+                tagColors.appendChild(tagColor);
+            }
+        }
+    });
+
+    tag.appendChild(tagTitle);
+    tag.appendChild(tagColors);
+    tagsHolder.appendChild(tag);
+}
+
+function getIndex(el) {
+    let children = el.parentNode.childNodes;
+    
+    for (let i = 0; i < children.length; i++) {
+        if (children[i] === el) {
+            return i;
+        }
+    }
+
+    return -1;
 }
 
 init();
